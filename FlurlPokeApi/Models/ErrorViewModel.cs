8a20757bbfeeec161a0dd123e2f0aa4113c0b580
Felipe Messa <51674627+messafelipe@users.noreply.github.com>
2024-04-21 15:22:55 -0300

    Alteração do projeto para API Web ASP. NET Core

--- FlurlPokeApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlurlPokeApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
